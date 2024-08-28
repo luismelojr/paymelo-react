a2a6fb24b7a02cbe486735f0988f286b6492ed36
Luis Henrique <junimhs1996@gmail.com>
2024-08-28 17:56:17 -0300

    chore: install shadcn

---
+++ resources/js/pages/Auth/Login.tsx
@@ -0,0 +1,7 @@
+
+export default function Login() {
+
+    return (
+        <h1 className={'text-muted-foreground'}>Hello World</h1>
+    );
+}
---
+++ resources/js/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function Welcome() {
+
+    return (
+        <div className={'p-4'}>
+            <h1 className={'text-muted'}>Welcome</h1>
+            <Button variant={'outline'}>Teste asas</Button>
+        </div>
+    );
+}
