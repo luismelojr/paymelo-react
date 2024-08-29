ae6f76881fe4de322dcbef84c5a9f1f567e0a300
Luis Henrique <junimhs1996@gmail.com>
2024-08-29 10:55:50 -0300

    feat: create toast service

--- resources/js/components/layouts/AuthLayout.tsx
+++ resources/js/components/layouts/AuthLayout.tsx
@@ -2,30 +2,35 @@ import { Link } from '@inertiajs/react'
 import { Wallet } from 'lucide-react'
 import { PropsWithChildren } from 'react'
 
+import Toast from '@/components/ui/toast'
+
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      <div className={'p-4 mt-10 flex flex-col'}>
-        <Link
-          href={route('login')}
-          className="flex items-center gap-2 text-2xl text-foreground"
-        >
-          <Wallet size={30} />
-          paymelo
-        </Link>
-        {children}
-        <div className={'mt-auto text-center'}>
-          <a
-            href="https://www.linkedin.com/in/luis-henrique-da-silva-melo-junior-416579155/"
-            target="_blank"
-            className="text-sm text-muted-foreground hover:text-foreground transition-all"
-            rel="noreferrer"
+    <>
+      <div className="h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className={'p-4 mt-10 flex flex-col'}>
+          <Link
+            href={route('login')}
+            className="flex items-center gap-2 text-2xl text-foreground"
           >
-            Desenvolvido por Luis Melo
-          </a>
+            <Wallet size={30} />
+            paymelo
+          </Link>
+          {children}
+          <div className={'mt-auto text-center'}>
+            <a
+              href="https://www.linkedin.com/in/luis-henrique-da-silva-melo-junior-416579155/"
+              target="_blank"
+              className="text-sm text-muted-foreground hover:text-foreground transition-all"
+              rel="noreferrer"
+            >
+              Desenvolvido por Luis Melo
+            </a>
+          </div>
         </div>
+        <div className="bg-[url('/assets/images/banner.jpg')] bg-no-repeat bg-cover bg-center relative hidden md:block lg:col-span-2" />
       </div>
-      <div className="bg-[url('/assets/images/banner.jpg')] bg-no-repeat bg-cover bg-center relative hidden md:block lg:col-span-2" />
-    </div>
+      <Toast />
+    </>
   )
 }
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,8 +5,16 @@ export interface User {
     email_verified_at?: string;
 }
 
+export interface Toast {
+    duration: number
+    text: string
+    type: string
+    description: string | null
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
+        toasts: Toast[];
     };
 };
