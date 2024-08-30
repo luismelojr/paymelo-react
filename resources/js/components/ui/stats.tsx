721af278ceffc4931e820b6053d7a2cc6569d4e1
Luis Henrique <junimhs1996@gmail.com>
2024-08-30 09:26:42 -0300

    feat: create stats component

---
+++ resources/js/components/ui/stats.tsx
@@ -0,0 +1,41 @@
+import { DollarSign, EyeOff } from 'lucide-react'
+import { useState } from 'react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+interface StatsProps {
+  title: string
+  value: string
+  showValue: boolean
+}
+
+export default function Stats({ title, value, showValue }: StatsProps) {
+  const [showValues, setShowValues] = useState(showValue)
+  return (
+    <Card x-chunk="dashboard-01-chunk-0">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <DollarSign className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <button
+          className="text-2xl font-bold"
+          onClick={() => setShowValues(!showValues)}
+        >
+          {showValues ? (
+            `${value}`
+          ) : (
+            <div
+              className={
+                'flex gap-2 items-center text-muted-foreground text-sm'
+              }
+            >
+              <EyeOff className="h-4 w-4 text-muted-foreground" />
+              <span>Mostar valor</span>
+            </div>
+          )}
+        </button>
+      </CardContent>
+    </Card>
+  )
+}
--- resources/js/pages/Dashboard.tsx
+++ resources/js/pages/Dashboard.tsx
@@ -1,106 +1,37 @@
-import { DollarSign, EyeOff } from 'lucide-react'
-import { useState } from 'react'
-
 import DashboardLayout from '@/components/layouts/DashboardLayout'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import Stats from '@/components/ui/stats'
 import useUser from '@/hooks/useUser'
 
 export default function Dashboard() {
   const user = useUser()
-  const [showValueReceipts, setShowValueReceipts] = useState(
-    user.config.hide_values,
-  )
-  const [showValueExpenses, setShowValueExpenses] = useState(
-    user.config.hide_values,
-  )
-  const [showValueBalance, setShowValueBalance] = useState(
-    user.config.hide_values,
-  )
   return (
     <DashboardLayout>
       <main className={'flex flex-col'}>
         <div className={'grid grid-cols-1 md:grid-cols-3 gap-4'}>
-          <Card x-chunk="dashboard-01-chunk-0">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Receitas do mês
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <button
-                className="text-2xl font-bold"
-                onClick={() => setShowValueReceipts(!showValueReceipts)}
-              >
-                {showValueReceipts ? (
-                  'R$ 10.000,00'
-                ) : (
-                  <div
-                    className={
-                      'flex gap-2 items-center text-muted-foreground text-sm'
-                    }
-                  >
-                    <EyeOff className="h-4 w-4 text-muted-foreground" />
-                    <span>Mostar valor</span>
-                  </div>
-                )}
-              </button>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-01-chunk-0">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Despesas do mês
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <button
-                className="text-2xl font-bold"
-                onClick={() => setShowValueExpenses(!showValueExpenses)}
-              >
-                {showValueExpenses ? (
-                  'R$ 10.000,00'
-                ) : (
-                  <div
-                    className={
-                      'flex gap-2 items-center text-muted-foreground text-sm'
-                    }
-                  >
-                    <EyeOff className="h-4 w-4 text-muted-foreground" />
-                    <span>Mostar valor</span>
-                  </div>
-                )}
-              </button>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-01-chunk-0">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Saldo do mês
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <button
-                className="text-2xl font-bold"
-                onClick={() => setShowValueBalance(!showValueBalance)}
-              >
-                {showValueBalance ? (
-                  'R$ 10.000,00'
-                ) : (
-                  <div
-                    className={
-                      'flex gap-2 items-center text-muted-foreground text-sm'
-                    }
-                  >
-                    <EyeOff className="h-4 w-4 text-muted-foreground" />
-                    <span>Mostar valor</span>
-                  </div>
-                )}
-              </button>
-            </CardContent>
-          </Card>
+          <Stats
+            title={'Receita'}
+            value={Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(1500)}
+            showValue={user.config.hide_values}
+          />
+          <Stats
+            title={'Despesas'}
+            value={Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(0)}
+            showValue={user.config.hide_values}
+          />
+          <Stats
+            title={'Despesas'}
+            value={Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(0)}
+            showValue={user.config.hide_values}
+          />
         </div>
       </main>
     </DashboardLayout>
